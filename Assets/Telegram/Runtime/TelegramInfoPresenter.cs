69026445ce7efdaf9804e058ca485b2a470a4101
Ehtesham <ehtesham@carameltec.com>
2025-03-11 03:29:27 +0500

    - telegram namespace and plugins

---
+++ Assets/Telegram/Runtime/TelegramInfoPresenter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Telegram.Runtime
+{
+    public sealed class TelegramInfoPresenter : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("TelegramWebApp.IsTelegramWebApp(): " + TelegramWebApp.IsTelegramWebApp());
+            Debug.Log("TelegramWebApp.GetInitData(): " + TelegramWebApp.GetInitData());
+        }
+    }
+}
